2984aa57b5c0ad691e81fa01340e4a04902a4c50
mainak124 <mainak124@gmail.com>
2016-11-28 00:49:17 -0800

    cp2 results

--- cp2/results/script.py
+++ cp2/results/script.py
@@ -14,8 +14,27 @@ def plot():
 	plt.xlabel("Log Array Size")
 	plt.show()
 
-def getAvg():
+def getAvg(lower, upper):
 	p_list = [16, 32, 64, 128, 256]
-	p, x_, y = np.loadtxt("mem_latency.txt", delimiter=",", unpack=True)
-	x = np.log(x_)/np.log(2)
+	p, x_, y = np.loadtxt("pagefault.txt", delimiter=",", unpack=True)
+	# x = np.log(x_)/np.log(2)
+	memaccess=[]
+	bigarr=[]
+	for p_ in  p_list:
+		memarr = [(float(b)/(100000)) for a,b,c in zip(x_,y,p) if c==p_ and a>lower and a<=upper]
+		bigarr = bigarr+memarr
+		memaccess.append(np.mean(memarr))
+	# print np.mean(memaccess)
+	print np.mean(bigarr)
+	print np.std(bigarr)#/np.mean(bigarr)
 
+l1_cache=0
+l2_cache=8192
+memory=65536
+
+print "For L1:"
+getAvg(l1_cache, l2_cache)
+print "For L2:"
+getAvg(l2_cache, memory)
+print "For Main Memory:"
+getAvg(memory, 100000000000000000000000)
